3e101dfab6c84f312aa8ebc0c915ab51227745a8
243f6a8885a308d313198a2e037 <243f6a8885a308d313198a2e037@gmail.com>
2020-06-05 16:48:36 +0900

    feat: added details to list_dir (become very slow)

--- snakeviz/main.py
+++ snakeviz/main.py
@@ -22,6 +22,27 @@ settings = {
 }
 
 
+class ListDirRow:
+    def __init__(self, path, href, file_size, execution_time, total_calls):
+        self.path = path
+        self.href = href
+        self.file_size = file_size
+        self.execution_time = execution_time
+        self.total_calls = total_calls
+
+    def to_cells(self):
+        return [{
+            'path': self.path,
+            'href': self.href
+        }, {
+            'file_size': self.file_size
+        }, {
+            'execution_time': self.execution_time
+        }, {
+            'total_calls': self.total_calls
+        }]
+
+
 class VizHandler(tornado.web.RequestHandler):
     def get(self, profile_name):
         abspath = os.path.abspath(profile_name)
@@ -41,12 +62,15 @@ class VizHandler(tornado.web.RequestHandler):
         Show a directory listing.
 
         """
-        entries = os.listdir(path)
-        dir_entries = [[[
-            '..',
-            quote(os.path.normpath(os.path.join(path, '..')), safe='')
-        ]]]
-        for name in entries:
+        dir_entries = []
+        show_details = True
+
+        row_root = ListDirRow(
+            '..', quote(os.path.normpath(os.path.join(path, '..')), safe=''),
+            None, None, None)
+        dir_entries.append(row_root.to_cells())
+
+        for name in os.listdir(path):
             if name.startswith('.'):
                 # skip invisible files/directories
                 continue
@@ -57,8 +81,24 @@ class VizHandler(tornado.web.RequestHandler):
                 displayname += '/'
             if os.path.islink(fullname):
                 displayname += '@'
-            dir_entries.append(
-                [[displayname, quote(os.path.join(path, linkname), safe='')]])
+            size = os.path.getsize(fullname)
+            total_calls = None
+            execution_time = None
+            if show_details and not os.path.isdir(fullname):
+                try:
+                    s = Stats(fullname)
+                    total_calls = s.total_calls
+                    execution_time = s.total_tt
+                except ValueError:
+                    pass
+                except:
+                    import traceback
+                    traceback.print_exc()
+                    pass
+            row_file = ListDirRow(
+                displayname, quote(os.path.join(path, linkname), safe=''),
+                size, execution_time, total_calls)
+            dir_entries.append(row_file.to_cells())
 
         self.render(
             'dir.html', dir_name=path, dir_entries=json.dumps(dir_entries))
